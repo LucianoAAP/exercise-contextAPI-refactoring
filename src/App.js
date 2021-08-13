40a1dd6ff8252ec1e3b0afc75103a617f898dc1e
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-08-13 17:26:36 -0300

    ex 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './context/MyContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      },
+    }
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(car, side) {
+    this.setState((prevState) => ({ cars: { ...prevState.cars, [car]: side } }));
+  }
+
+  render() {
+    return (
+      <MyContext.Provider value={ { ...this.state, moveCar: this.moveCar } }>
+        <Cars />
+      </MyContext.Provider>
+    );
+  }
 }
 
 export default App;
